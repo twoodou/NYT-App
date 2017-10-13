1a1c1753a441ee74bdd8424236252ca36a73a461
Robert Paul Trettenero <Tretteneropaul@yahoo.com>
2017-10-13 13:20:52 -0500

    Modified index.hml and created a js template

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+var term = $(".termField").val();				//This takes whatevers in the term fieldbox
+var num = $(".numField").val();				//This takes whatevers in the number of records fieldbox
+var startYear = $(".startYearField").val();				//This takes whatevers in the start year fieldbox
+var endYear = $(".endYearField").val();				//This takes whatevers in the end year fieldbox
+
+
+$(".search").on("click", function(event) {		//When you click dat search button
+	console.log(term);	//testing if the variable are saving correctly 
+	console.log(num);	 
+	console.log(startYear);	//REPLACE this with 
+	console.log(endYear);	//REPLACE this with 
+
+});
+/*
+for(var i = 0; i < ; i++){
+
+}*/
